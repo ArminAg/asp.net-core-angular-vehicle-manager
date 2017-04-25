ed3090f56cb8432438784071fe88004fa809972a
ArminAg <armin.ag@outlook.com>
2017-04-25 21:34:52 +0200

    Seed Database

---
+++ Migrations/20170425191740_SeedDatabase.Designer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using asp.net_core_angular_vehicle_manager.Persistence;
+
+namespace AspNetCoreAngularVehicleManager.Migrations
+{
+    [DbContext(typeof(VehicleManagerDbContext))]
+    [Migration("20170425191740_SeedDatabase")]
+    partial class SeedDatabase
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("asp.net_core_angular_vehicle_manager.Core.Models.Make", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(255);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Makes");
+                });
+
+            modelBuilder.Entity("asp.net_core_angular_vehicle_manager.Core.Models.Model", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("MakeId");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(255);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("MakeId");
+
+                    b.ToTable("Models");
+                });
+
+            modelBuilder.Entity("asp.net_core_angular_vehicle_manager.Core.Models.Model", b =>
+                {
+                    b.HasOne("asp.net_core_angular_vehicle_manager.Core.Models.Make", "Make")
+                        .WithMany("Models")
+                        .HasForeignKey("MakeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
