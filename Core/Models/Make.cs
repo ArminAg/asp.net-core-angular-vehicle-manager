621abacc95e69c7a121837576f69813bf5186a91
ArminAg <armin.ag@outlook.com>
2017-04-25 21:16:36 +0200

    Apply Model Contraints

--- Core/Models/Make.cs
+++ Core/Models/Make.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace asp.net_core_angular_vehicle_manager.Core.Models
 {
     public class Make
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public ICollection<Model> Models { get; set; }
 
--- Core/Models/Model.cs
+++ Core/Models/Model.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace asp.net_core_angular_vehicle_manager.Core.Models
 {
     public class Model
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public Make Make { get; set; }
         public int MakeId { get; set; }
