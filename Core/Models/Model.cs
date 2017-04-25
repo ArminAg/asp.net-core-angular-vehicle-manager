74f1b5c104012cc4868e834a2aed9457c08de2d4
ArminAg <armin.ag@outlook.com>
2017-04-25 19:43:02 +0200

    Build Domain Model

---
+++ Core/Models/Model.cs
@@ -0,0 +1,10 @@
+namespace asp.net_core_angular_vehicle_manager.Core.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
+    }
+}
