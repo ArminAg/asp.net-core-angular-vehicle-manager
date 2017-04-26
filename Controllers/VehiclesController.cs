eadbfe84fa4ac4943e55270c91dd74227cf4f67a
ArminAg <armin.ag@outlook.com>
2017-04-26 13:48:24 +0200

    Implement Vehicle Collection Mapping

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -5,6 +5,7 @@ using asp.net_core_angular_vehicle_manager.Core.Models;
 using asp.net_core_angular_vehicle_manager.Persistence;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace asp.net_core_angular_vehicle_manager.Controllers
 {
@@ -36,6 +37,29 @@ namespace asp.net_core_angular_vehicle_manager.Controllers
             vehicle.LastUpdate = DateTime.Now;
 
             context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var model = await context.Models.FindAsync(vehicleResource.ModelId);
+            if (model == null)
+            {
+                ModelState.AddModelError("ModelId", "Invalid modelId.");
+                return BadRequest(ModelState);
+            }
+
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
             await context.SaveChangesAsync();
 
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -2,6 +2,7 @@ using asp.net_core_angular_vehicle_manager.Controllers.Resources;
 using asp.net_core_angular_vehicle_manager.Core.Models;
 using AutoMapper;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace asp.net_core_angular_vehicle_manager.Mapping
 {
@@ -19,10 +20,23 @@ namespace asp.net_core_angular_vehicle_manager.Mapping
 
             // API Resource to Domain
             CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.Id, opt => opt.Ignore())
                 .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
                 .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
                 .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
-                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
+                .ForMember(v => v.Features, opt => opt.Ignore())
+                .AfterMap((vr, v) =>
+                {
+                    // Remove unselected features
+                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId));
+                    foreach (var f in removedFeatures)
+                        v.Features.Remove(f);
+
+                    // Add new features
+                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature { FeatureId = id });
+                    foreach (var f in addedFeatures)
+                        v.Features.Add(f);
+                });
         }
     }
 }
