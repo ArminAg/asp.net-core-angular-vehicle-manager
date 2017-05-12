2d2ab0e71b286062db7c269c2cbc439495b3eb68
ArminAg <armin.ag@outlook.com>
2017-05-12 20:54:24 +0200

    Bind Checkboxes

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -10,7 +10,9 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];
   features: any[];
   models: any[];
-  vehicle: any = {};
+  vehicle: any = {
+    features: []
+  };
 
   constructor(
     private vehicleService: VehicleService) { }
@@ -24,5 +26,14 @@ export class VehicleFormComponent implements OnInit {
     var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
     this.models = selectedMake ? selectedMake.models : [];
     delete this.vehicle.modelId;
+  }
+
+  onFeatureToggle(featureId, $event) {
+    if ($event.target.checked) {
+      this.vehicle.features.push(featureId);
+    } else {
+      var index = this.vehicle.features.indexOf(featureId);
+      this.vehicle.features.splice(index, 1);
+    }
   }
 }
