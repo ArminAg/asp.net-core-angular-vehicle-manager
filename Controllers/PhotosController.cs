abc01f5e96bf000cb70d6751f286a11de10769a3
ArminAg <armin.ag@outlook.com>
2017-06-27 22:49:09 +0100

    Add photo upload validation

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using asp.net_core_angular_vehicle_manager.Controllers.Resources;
 using asp.net_core_angular_vehicle_manager.Core;
@@ -15,6 +16,9 @@ namespace asp.net_core_angular_vehicle_manager.Controllers
     [Route("/api/vehicles/{vehicleId}/photos")]
     public class PhotosController : Controller
     {
+        private readonly int MAX_BYTES = 1 * 1024 * 1024;
+        private readonly string[] ACCEPTED_FILE_TYPES = new[] { ".jpg", ".jpeg", ".png" };
+
         private readonly IHostingEnvironment host;
         private readonly IVehicleRepository vehicleRepository;
         private readonly IUnitOfWork unitOfWork;
@@ -34,6 +38,11 @@ namespace asp.net_core_angular_vehicle_manager.Controllers
             var vehicle = await vehicleRepository.GetVehicle(vehicleId, includeRelated: false);
             if (vehicle == null)
                 return NotFound();
+
+            if (file == null) return BadRequest("Null file");
+            if (file.Length == 0) return BadRequest("Empty file");
+            if (file.Length > MAX_BYTES) return BadRequest("Max file size exceeded");
+            if (!ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName))) return BadRequest("Invalid file type");
 
             var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
             if (!Directory.Exists(uploadsFolderPath))
